739ae7f572aafc58b9d9db976ad0247005171f22
Thomas Monfre <tmonfre@users.noreply.github.com>
2018-10-24 23:32:03 -0400

    temp historical data adjusts to user interactions

--- pb-frontend/src/scripts/viewdata.js
+++ pb-frontend/src/scripts/viewdata.js
@@ -1,34 +1,24 @@
-// user selections for state, region, and whether or not to view data for all
-// of the US or just a specific state-region combination
+// this file contains all elements necessary to visualize data on the view data page
+// we hold data from the server in totalData and currentData
+// state, region, startDate and endDate are user selections on screen
+// stateRegionMap holds all states and regions that should be user selection options
+// helper methods are defined below
+
+// user selections for state, region, and whether or not to view data for all of the US or just a specific state-region combination
 var state = null;
 var region = null;
 var showUS = true;
 
-var totalData; // entire dataset available to the user
-getData("historical_data.json");
-console.log(totalData);
+// user selections for date range
+var startDate = Infinity;
+var endDate = 0;
 
-var currentData = totalData; // just the data that the user wants to look at based on current selection
+// entire dataset available to the user
+var totalData = [{"_id":"5bcb7928763d69464cb8d23e","yearNumber":1,"state":"AL","nf":"BANKHEAD","classification":"RD","forest":"BANKHEAD RD","stateCode":1,"forestCode":1,"latitude":34.23306,"longitude":-87.31112,"host":20.68,"year":1986,"spbPerTwoWeeks":null,"cleridsPerTwoWeeks":null,"spots":564,"spotsPerHundredKm":273,"percentSpb":null,"__v":0,"id":"5bcb7928763d69464cb8d23e"},{"_id":"5bcb7928763d69464cb8d23f","yearNumber":2,"state":"AL","nf":"BANKHEAD","classification":"RD","forest":"BANKHEAD RD","stateCode":1,"forestCode":1,"latitude":34.23306,"longitude":-87.31112,"host":20.68,"year":1987,"spbPerTwoWeeks":931,"cleridsPerTwoWeeks":210,"spots":600,"spotsPerHundredKm":290,"percentSpb":null,"__v":0,"id":"5bcb7928763d69464cb8d23f"},{"_id":"5bcb7928763d69464cb8d241","yearNumber":4,"state":"AL","nf":"BANKHEAD","classification":"RD","forest":"BANKHEAD RD","stateCode":1,"forestCode":1,"latitude":34.23306,"longitude":-87.31112,"host":20.68,"year":1989,"spbPerTwoWeeks":390,"cleridsPerTwoWeeks":397,"spots":45,"spotsPerHundredKm":22,"percentSpb":null,"__v":0,"id":"5bcb7928763d69464cb8d241"}];
+//getData("historical_data.json");
 
-// fetch json data
-function getData(url) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            totalData = JSON.parse(this.responseText);
-        }
-    };
-    console.log("Done!");
-    xmlhttp.open("GET", "historical_data.json", true);
-    xmlhttp.send();
-};
-
-
-
-
-// user selection for start and end date they would like to view data on
-var startDate = 1987;
-var endDate = new Date().getFullYear(); // current year
+// just the data that the user wants to look at based on current selection
+var currentData = totalData;
 
 // this holds all possible states that data exists for based on the user-selected years
 // all array elements in index 1 or greater represent associated regions for that state
@@ -51,32 +41,178 @@ var stateRegionMap = {
     VA:["Virginia"]
 }
 
-// refresh the list of available states and regions on the screen
+// update start and end dates initially available to user then refresh list of available states and regions
+updateStartAndEndDateFromCurrentData();
 refreshSelectionMenus();
+
+// updates currentData that will be visualized on screen
+function refreshCurrentData() {
+    var newSet = [];
+    // iterate through data and adjust for date range and state/region selection
+    for (i in totalData) {
+        if ((totalData[i].year >= startDate && totalData[i].year <= endDate) && (state != null && totalData[i].state === state) && (region != null && totalData[i].nf === region)) {
+            newSet.push(totalData[i]);
+        }
+    }
+    currentData = newSet;
+    refreshSelectionMenus();
+}
+
+// fetch json data and put into totalData
+function getData(url) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            totalData = JSON.parse(this.responseText);
+        }
+    };
+    console.log("Done!");
+    xmlhttp.open("GET", "historical_data.json", true);
+    xmlhttp.send();
+};
+
+// updates the start and end dates for the available data
+function updateStartAndEndDateFromCurrentData() {
+    for (obj in currentData) {
+        // set startDate
+        if (currentData[obj].year < startDate) {
+            startDate = currentData[obj].year;
+        }
+        // set endDate
+        if (currentData[obj].year > endDate) {
+            endDate = currentData[obj].year;
+        }
+    }
+
+    // TODO: update the dom here for start and end date initially available to user
+
+}
+
+// trigger function for when user adjusts start date
+function updateStartDate(year) {
+    startDate = year;
+    refreshCurrentData();
+}
+
+// trigger function for when user adjusts end date
+function updateEndDate(year) {
+    endDate = year;
+    refreshCurrentData();
+}
+
+// trigger function for when user adjusts state
+function changeStateSelection() {
+    if (showUS) {
+        showUS = false;
+
+        // update button coloring
+        document.getElementById("select-buttons").children[0].removeAttribute("id");
+        document.getElementById("select-buttons").children[1].setAttribute("id","active");
+
+        // show region area
+        document.getElementById("region").style.display = "block";
+    }
+
+    // update global variable to the newly selected state and get stateName
+    state = document.getElementById('state-select').value
+    stateName = stateRegionMap[state][0];
+
+    // update the drop down menu of new regions then select the first region
+    updateRegionDropDown();
+    document.getElementById('region-select').selectedIndex = "1";
+    region = document.getElementById('region-select').value
+
+    // update all text fields on screen
+    updateStateTextFields(stateName);
+    updateRegionTextFields();
+
+    // refresh the data available for analysis
+    refreshCurrentData();
+}
+
+// trigger function for when user adjusts region
+function changeRegionSelection() {
+    region = document.getElementById('region-select').value
+    updateRegionTextFields();
+
+    // refresh the data available for analysis
+    refreshCurrentData();
+}
+
+// trigger function for when user wants to switch between viewing data for all US or just a specific state
+function switchNationSelection(newSelect) {
+
+    // if the user wants to see only metrics for the United States
+    if (newSelect === "us") {
+        showUS = true;
+
+        // update button coloring
+        document.getElementById("select-buttons").children[0].setAttribute("id","active");
+        document.getElementById("select-buttons").children[1].removeAttribute("id");
+
+        // hide region area
+        document.getElementById("region").style.display = "none";
+
+        // change state to United States
+        textFields = document.getElementsByClassName("state-text");
+        for (i = 0; i < textFields.length; i++) {
+            textFields[i].innerHTML = "United States";
+        }
+
+        // change selection menus to default selection
+        document.getElementById('state-select').selectedIndex = "0";
+        document.getElementById('region-select').selectedIndex = "0";
+    }
+    // if the user wants to see metrics for a specific location
+    else {
+        showUS = false;
+
+        // update button coloring
+        document.getElementById("select-buttons").children[0].removeAttribute("id");
+        document.getElementById("select-buttons").children[1].setAttribute("id","active");
+
+        // show region area
+        document.getElementById("region").style.display = "block";
+
+        // refresh the list of available states and regions on the screen
+        refreshSelectionMenus();
+
+        // update selection menus and text fields
+        document.getElementById('state-select').selectedIndex = "1";
+        changeStateSelection();
+    }
+
+    // refresh the data available for analysis
+    refreshCurrentData();
+}
 
 // refreshes the selection menus based on the available data for the desired dates, state, and region
 function refreshSelectionMenus() {
-    updateAvailableStatesAndRegions();
+    updateAvailableStatesAndRegionsInDropDown();
     clearSelectionOptionNodes();
     populateSelectionMenus();
 }
 
-// update the map of available states and regions based on user selected years
-// TODO this function will add/remove available states and regions based on the data that the user has selected and wishes to view
-function updateAvailableStatesAndRegions() {
+// update the map of available states and regions to be placed in drop down menu based on current data
+// a state will not appear as a selection option if it does not have any associated regions
+function updateAvailableStatesAndRegionsInDropDown() {
+    // clear stateRegionMap
+    for (stateAbbrev in stateRegionMap) {
+        stateRegionMap[stateAbbrev] = [stateRegionMap[stateAbbrev][0]];
+    }
 
-    // a state will not appear as a selection option if it does not have any associated regions
-    // to clear a state and indicate that it should not show up on the screen, remove all elements in it's associated array in the map except index 0 (the state name)
-
-
-
+    // update stateRegionMap based on current data
+    for (obj in currentData) {
+        // if the state region map doesn't include this region, add it
+        if (!stateRegionMap[currentData[obj].state].includes(currentData[obj].nf)) {
+            stateRegionMap[currentData[obj].state].push(currentData[obj].nf);
+        }
+    }
 }
 
 // remove all option elements that are child nodes of the selection menus
 function clearSelectionOptionNodes() {
-
     // clear state selection menu
-
     // grab a reference to DOM state select node and its children
     stateSelectNode = document.getElementById('state-select');
     optionDivs = stateSelectNode.children;
@@ -87,7 +223,6 @@ function clearSelectionOptionNodes() {
     }
 
     // clear region selection menus
-
     // grab a reference to DOM region select node and its children
     regionSelectNode = document.getElementById('region-select');
     optionDivs = regionSelectNode.children;
@@ -100,7 +235,6 @@ function clearSelectionOptionNodes() {
 
 // creates option elements as children nodes to the selection menu for all available states and regions in the map
 function populateSelectionMenus() {
-
     // grab a reference to DOM state select node and its children
     stateSelectNode = document.getElementById('state-select');
     optionDivs = stateSelectNode.children;
@@ -123,7 +257,6 @@ function populateSelectionMenus() {
     if (state != null) {
         updateRegionDropDown();
     }
-
 }
 
 // update all region dropdown options in the DOM
@@ -150,88 +283,7 @@ function updateRegionDropDown() {
     }
 }
 
-
-
-// handle selection buttons between United States and Current Selection
-function switchNationSelection(newSelect) {
-
-    // if the user wants to see only metrics for the United States
-    if (newSelect === "us") {
-        showUS = true;
-
-        // update button coloring
-        document.getElementById("select-buttons").children[0].setAttribute("id","active");
-        document.getElementById("select-buttons").children[1].removeAttribute("id");
-
-        // hide region area
-        document.getElementById("region").style.display = "none";
-
-        // change state to United States
-        textFields = document.getElementsByClassName("state-text");
-        for (i = 0; i < textFields.length; i++) {
-            textFields[i].innerHTML = "United States";
-        }
-
-        // change selection menus to default selection
-        document.getElementById('state-select').selectedIndex = "0";
-        document.getElementById('region-select').selectedIndex = "0";
-
-    }
-    // if the user wants to see metrics for a specific location
-    else {
-        showUS = false;
-
-        // update button coloring
-        document.getElementById("select-buttons").children[0].removeAttribute("id");
-        document.getElementById("select-buttons").children[1].setAttribute("id","active");
-
-        // show region area
-        document.getElementById("region").style.display = "block";
-
-        // refresh the list of available states and regions on the screen
-        refreshSelectionMenus();
-
-        // update selection menus and text fields
-        document.getElementById('state-select').selectedIndex = "1";
-        changeStateSelection();
-    }
-}
-
-// handle state selection
-function changeStateSelection() {
-
-    if (showUS) {
-        showUS = false;
-
-        // update button coloring
-        document.getElementById("select-buttons").children[0].removeAttribute("id");
-        document.getElementById("select-buttons").children[1].setAttribute("id","active");
-
-        // show region area
-        document.getElementById("region").style.display = "block";
-    }
-
-    // update global variable to the newly selected state and get stateName
-    state = document.getElementById('state-select').value
-    stateName = stateRegionMap[state][0];
-
-    // update the drop down menu of new regions then select the first region
-    updateRegionDropDown();
-    document.getElementById('region-select').selectedIndex = "1";
-    region = document.getElementById('region-select').value
-
-    // update all text fields on screen
-    updateStateTextFields(stateName);
-    updateRegionTextFields();
-}
-
-// handle region selection
-function changeRegionSelection() {
-    region = document.getElementById('region-select').value
-    updateRegionTextFields();
-}
-
-// change dom elements to new state selection
+// helper function: change dom elements to new state selection
 function updateStateTextFields(stateName) {
     textFields = document.getElementsByClassName("state-text");
 
@@ -240,7 +292,7 @@ function updateStateTextFields(stateName) {
     }
 }
 
-// change dom elements to new region selection
+// helper function: change dom elements to new region selection
 function updateRegionTextFields() {
     textFields = document.getElementsByClassName("region-text");
 
