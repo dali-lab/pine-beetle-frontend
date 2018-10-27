35f2c6220f7d94e0505ccecdf6fd6bdc42a9193b
Isabel Hurley <isabel.hurley@dali.dartmouth.edu>
2018-10-27 17:41:42 -0400

    in progress: deploy front end (import all static resources necessary in index.js, set app path as src/scripts/index)

--- pb-frontend/src/scripts/index.js
+++ pb-frontend/src/scripts/index.js
@@ -1,3 +1,23 @@
+import '../styles/index.css';
+import '../styles/default.css';
+import '../styles/about.css';
+import '../styles/viewdata.css';
+
+import './default.js';
+import './map.js';
+import './viewdata.js';
+
+import '../images/dali.png';
+import '../images/dali_blue_green.png';
+import '../images/dali_dark.png';
+import '../images/dartmouth.png';
+import '../images/future_des_1.png';
+import '../images/future_des_2.png';
+import '../images/sample_img_1.jpeg';
+import '../images/sample_img_2.jpg';
+import '../images/sample_img_3.jpeg';
+import '../images/usfs.png';
+
 var selection = "us";
 
 function changeSelection(newSelect) {
