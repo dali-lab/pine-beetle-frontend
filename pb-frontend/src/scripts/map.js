0ec7bb33363ba0bada7fe39970397c5b1c9da6fd
Thomas Monfre <tmonfre@users.noreply.github.com>
2018-10-29 15:03:45 -0400

    des changes and loading animation

--- pb-frontend/src/scripts/map.js
+++ pb-frontend/src/scripts/map.js
@@ -10,7 +10,7 @@ function(Map, MapView) {
   var view = new MapView({
     container: "viewDiv",  // Reference to the DOM node that will contain the view
     map: map,              // References the map object created in step 3
-    zoom: 4,  // Sets zoom level based on level of detail (LOD)
-    center: [15, 65]  // Sets center point of view using longitude,latitude
+    zoom: 5.5,  // Sets zoom level based on level of detail (LOD)
+    center: [-84.3880, 33.7490]  // Sets center point of view using longitude,latitude
   });
 });
