5800cd32dcb05c5c1e3dca7a8273e11a0e8d51fc
Isabel Hurley <isabel.hurley@dali.dartmouth.edu>
2018-11-11 21:53:40 -0500

    functions to get data on the frontend

---
+++ pb-frontend/src/scripts/importFromMongo.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+const localURL = "http://localhost:9090/v1/"
+//TODO setup switch between local and deployed URL
+
+const getHistoricalData = async () => {
+  try {
+    return axios.get(localURL+'getHistoricals')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const historicalData = getHistoricalData().then((data) => {
+	console.log(data)
+}).catch((err) => {
+	console.log(err);
+})
+
+const getSpotData = async () => {
+	try {
+		return axios.get(localURL+'getSpots')
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+const getBeetleData = async () => {
+	try {
+		return axios.get(localURL + 'getBeetles')
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+const dataController = { getHistoricalData, getSpotData, getBeetleData }
+export default dataController;
+
+// const spotData = getSpotData().then((data) => {
+// 	console.log(data)
+// }).catch((err) => {
+// 	console.log(err);
+// })
+
+// const beetleData = getBeetleData().then((data) => {
+// 	console.log(data)
+// }).catch((err) => {
+// 	console.log(err);
+// })
