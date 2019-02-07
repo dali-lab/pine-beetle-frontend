a2ccb125ebf8479ddd2cbef0d14352d5a7dcd1e1
m-hess <madeline.e.hess.19@dartmouth.edu>
2019-02-07 18:30:43 -0500

    Text changes

--- src/components/about-page/About.js
+++ src/components/about-page/About.js
@@ -24,9 +24,24 @@ class About extends Component {
     				<div className="container next-steps flex-item flex-item-left">
     					<h2>Partners and Cooperators</h2>
     					<div className="line"></div>
-    					<p>Moving forward, this tool will show rich data visualizations that allow users to drill into data and understand pine beetle movements in their region. It will also be updated to include a more flexible and fully developed map visualization showing outbreak densities that are more intuitive and easier to understand. Furthermore, this tool will provide spreadsheet visualizations, allowing users to export .csv files to further explore the data. Additionally, the statistical model used to predict Southern Pine Beetle outbreaks will continue to be enhanced and updated to accurately predict pine beetle movements.</p>
-    					<p>Be sure to come back to this site often to check up on progress being made. We look forward to deploying a product that allows USFS researchers, government agencies, and even the general public to explore changes happening throughout the nation in the near future.</p>
-    				</div>
+
+              <p>The southern pine beetle prediction system is a collaboration of scores of people and institutions.</p>
+
+              <h3>Project Committee:</h3>
+              <p>Matthew Ayres, Carissa Aoki, Steve Clark, Kevin Dodds, Lorie Loeb, John Nowak, Brian Sullivan, Michael Torbett</p>
+              <p>The creation of this resource for delivering SPB outbreak predictions was supported by a grant from the U.S.D.A. Forest Service Special Technology Development Program (STDP) to MP Ayres and CF Aoki at Dartmouth College.</p>
+
+              <h3>U.S.D.A. Forest Service: </h3>
+              <p>Steve Clark, Entomologist; John Nowak, Entomologist, Southern Pine Beetle Prevention Program Coordinator; Brian Sullivan, Entomologist</p>
+
+              <h3>State Cooperators: </h3>
+              <p>Alabama Forestry Commission: Dana Stone; Arkansas Forestry Commission: Chandler Barton; Florida Forest Service: Jeff Eickwort and Chris Pearce; Georgia Forestry Commission: Michael Torbett and Chip Bates; Louisiana Department of Agriculture and Forestry: Brent Cutrer; Mississippi Forestry Commission: Todd Matthews; North Carolina Forest Service: Rob Trickel; South Carolina Forestry Commission: David Jenkins; Tennessee Division of Forestry: Nathan Hoover; Texas A&M Forest Service: Shane Harrington; Virginia Department of Forestry: Lori Chamberlin</p>
+
+              <h3>DALI Lab:</h3>
+              <p>Web design and development by the Dartmouth Applied Learning and Innovation (DALI) Lab at Dartmouth College. Lorie Loeb, Director and Co-founder; Madeline Hess, Developer; Thomas Monfre, Developer; Isabel Hurley, Developer; Mo Zhu, Team Manager; Bella Jacoby, Designer; Emi Hayakawa, Designer</p>
+
+
+            </div>
 
                     <div className="container contact-info flex-item flex-item-right">
                         <h2>Contact Information</h2>
