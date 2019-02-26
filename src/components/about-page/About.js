2069c8e4afd2a7c376e9109705a241225cbf6884
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-02-26 01:35:44 -0500

    fixed font-size issues

--- src/components/about-page/About.js
+++ src/components/about-page/About.js
@@ -17,6 +17,7 @@ class About extends Component {
                         <div className="header-text-container">
                             <h3>History</h3>
                             <p>This project carries on the work of Ron Billings of the Texas A&M University Forest Service (retired). Since 1987, Ron has coordinated annual predictions of summer SPB outbreak, based on spring trapping data provided by cooperators across the southern range of the beetle (Billings and Upton 2010). Our work builds on Ron’s tireless dedication to supporting and improving the prediction system over the past 30 years. </p>
+                            <br />
                             <p>(Billings, R. F., and W. W. Upton. 2010. A methodology for assessing annual risk of southern pine beetle outbreaks across the southern region using pheromone traps. Pages 73–85 in J. M. Pye, H. M. Rauscher, Y. Sands, D. C. Lee, and J. S. Beatty, editors. Advances in threat assessment and their application to forest and rangeland management. Gen. Tech. Rep. PNW-GTR-802. Portland, OR: U.S. Department of Agriculture, Forest Service, Pacific Northwest and Southern Research Stations.)</p>
                         </div>
                         
@@ -26,23 +27,23 @@ class About extends Component {
                         </div>
 
                         <div className="header-text-container">
-                            <h3>Project Committee:</h3>
+                            <h4>Project Committee:</h4>
                             <p>Matthew Ayres, Carissa Aoki, Steve Clark, Kevin Dodds, Lorie Loeb, John Nowak, Brian Sullivan, Michael Torbett</p>
                             <p>The creation of this resource for delivering SPB outbreak predictions was supported by a grant from the U.S.D.A. Forest Service Special Technology Development Program (STDP) to MP Ayres and CF Aoki at Dartmouth College.</p>
                         </div>
 
                         <div className="header-text-container">
-                            <h3>U.S.D.A. Forest Service: </h3>
+                            <h4>U.S.D.A. Forest Service: </h4>
                             <p>Steve Clark, Entomologist; John Nowak, Entomologist, Southern Pine Beetle Prevention Program Coordinator; Brian Sullivan, Entomologist</p>
                         </div>
 
                         <div className="header-text-container">
-                            <h3>State Cooperators: </h3>
+                            <h4>State Cooperators: </h4>
                             <p>Alabama Forestry Commission: Dana Stone; Arkansas Forestry Commission: Chandler Barton; Florida Forest Service: Jeff Eickwort and Chris Pearce; Georgia Forestry Commission: Michael Torbett and Chip Bates; Louisiana Department of Agriculture and Forestry: Brent Cutrer; Mississippi Forestry Commission: Todd Matthews; North Carolina Forest Service: Rob Trickel; South Carolina Forestry Commission: David Jenkins; Tennessee Division of Forestry: Nathan Hoover; Texas A&M Forest Service: Shane Harrington; Virginia Department of Forestry: Lori Chamberlin</p>
                         </div>
 
                         <div className="header-text-container">
-                            <h3>DALI Lab:</h3>
+                            <h4>DALI Lab:</h4>
                             <p>Web design and development by the Dartmouth Applied Learning and Innovation (DALI) Lab at Dartmouth College. Lorie Loeb, Director and Co-founder; Madeline Hess, Developer; Thomas Monfre, Developer; Isabel Hurley, Developer; Mo Zhu, Team Manager; Bella Jacoby, Designer; Emi Hayakawa, Designer</p>
                         </div>
 
@@ -59,7 +60,7 @@ class About extends Component {
                         </div>
 
                         <div id="contact-info">
-                            <h2 id="contact-info-header">Contact Information</h2>
+                            <h3 id="contact-info-header">Contact Information</h3>
                             <div id="contact-info-area">
                                 <div className="info-box">
                                     <h4>Questions about the program</h4>
