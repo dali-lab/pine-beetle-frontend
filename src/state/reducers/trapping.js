9913828ca3989ecfff75f13107efa35ed3d95bed
Thomas Monfre <tmonfre1@gmail.com>
2020-10-12 19:13:04 -0400

    feat: update trapping and predictions reducer when data mode changes

--- src/state/reducers/trapping.js
+++ src/state/reducers/trapping.js
@@ -1,8 +1,10 @@
 import { ActionTypes } from '../actions';
+import { DATA_MODES } from '../../constants';
 
 const initialState = {
-  county: [],
-  rangerDistrict: [],
+  data: [], // current data (changes when user updates data mode, components should import this)
+  county: [], // the county data
+  rangerDistrict: [], // the ranger district data
 
   fetchingCounty: false,
   fetchingRangerDistrict: false,
@@ -21,6 +23,9 @@ const TrappingReducer = (state = initialState, action) => {
 
     case ActionTypes.FETCHING_RANGER_DISTRICT_TRAPPING:
       return { ...state, fetchingRangerDistrict: action.payload };
+
+    case ActionTypes.SET_DATA_MODE:
+      return { ...state, data: action.payload === DATA_MODES.COUNTY ? state.county : state.rangerDistrict };
 
     default:
       return state;
