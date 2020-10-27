90d06937308ad0292f3fcd48b26de6b7b442b90b
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2020-10-27 10:57:26 -0700

    histograms

--- src/screens/prediction/component.js
+++ src/screens/prediction/component.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 import {
@@ -10,6 +11,13 @@ import {
 
 import './style.scss';
 
+const histogrambin1 = require('../../assets/images/spb-histogram-bin1.jpg');
+const histogrambin2 = require('../../assets/images/spb-histogram-bin2.jpg');
+const histogrambin3 = require('../../assets/images/spb-histogram-bin3.jpg');
+const histogrambin4 = require('../../assets/images/spb-histogram-bin4.jpg');
+const histogrambin5 = require('../../assets/images/spb-histogram-bin5.jpg');
+const histogrambin6 = require('../../assets/images/spb-histogram-bin6.jpg');
+
 const Prediction = (props) => {
   const {
     isLoading,
@@ -18,10 +26,36 @@ const Prediction = (props) => {
     selectedState,
   } = props;
 
-  // eslint-disable-next-line no-unused-vars
-  // const [mapData, setMapData] = useState(null);
-  // console.log(selectedState);
-  // console.log(predictionData);
+  const getHistogram = (predProb50) => {
+    if (predProb50 < 0.025) {
+      return histogrambin1;
+    } else if (predProb50 < 0.05) {
+      return histogrambin2;
+    } else if (predProb50 < 0.15) {
+      return histogrambin3;
+    } else if (predProb50 < 0.25) {
+      return histogrambin4;
+    } else if (predProb50 < 0.4) {
+      return histogrambin5;
+    } else {
+      return histogrambin6;
+    }
+  };
+
+  const createHistogramJSX = (bool) => {
+    if (bool) {
+      const predProb50 = predictionData[0].prediction['prob.Spots>53'];
+      const histogram = getHistogram(predProb50);
+      return (
+        <img
+          src={histogram}
+          alt="Histogram for Predicted % Chance of >50 Spots"
+          style={{ width: '800px' }}
+          className="container"
+        />
+      );
+    } else { return null; }
+  };
 
   const predDetails = (bool) => {
     if (bool) {
@@ -41,11 +75,6 @@ const Prediction = (props) => {
     } else { return null; }
   };
 
-  // useEffect(() => {
-  //   setMapData(selectedState);
-  //   console.log('selected state updated');
-  // }, [selectedState]);
-
   return (
     <div>
       {/* TODO: make this a spinner */}
@@ -56,6 +85,7 @@ const Prediction = (props) => {
       <PredictionMap data={selectedState} />
       {/* TODO: Dynamically Change header */}
       { predDetails(predictionData.length === 1) }
+      { createHistogramJSX(predictionData.length === 1) }
     </div>
   );
 };
