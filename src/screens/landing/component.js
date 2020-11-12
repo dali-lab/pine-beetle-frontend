f8aba098f920dd473fe22bd4b4902074e6d4e2ed
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2020-11-12 15:48:29 -0800

    new feature: landing title area

--- src/screens/landing/component.js
+++ src/screens/landing/component.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './style.scss';
+
+const beetleIcon = require('../../assets/icons/background-beetle.png');
 
 const Landing = (_props) => {
   return (
-    <div id="home-outer-container">
-      <div id="home-landing-container">
-        <h1>Predicting Southern Pine Beetle Outbreaks</h1>
-        <p>This prototype exhibits the first steps in a months-long process to develop a fully deployed tool to visualize and predict
-          trends in Southern Pine Beetle movements throughout the United States.
-        </p>
-        <p id="home-last">See the historical data page for metrics on previous years and the predictive model page to generate expectations about the future.</p>
-        <Link to="/historical-data" id="hist-data-link" className="home-button">Historical Data</Link>
-        <Link to="/predictions" id="pred-model-link" className="home-button">Predictive Model</Link>
-        <p id="home-last" />
+    <div className="container">
+      <div className="title-area">
+        <img src={beetleIcon} alt="beetle" />
+        <div className="title-words">
+          <div id="big-title">Pine Beetle</div>
+          <div id="subtitle">Outbreak Prediction</div>
+          <div id="title-description">
+            This website predicts the likelihood of a summer outbreak based on spring trapping data, with the goal of assisting forest managers as they make resource allocation decisions.
+          </div>
+        </div>
       </div>
+      <div className="pages">links here</div>
+      <div className="video">Video here</div>
+      <div className="how-it-works">hi:)</div>
     </div>
   );
 };
