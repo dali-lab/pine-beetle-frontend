1a7035f656f2971d6ca8b94b4ce9049d29ea40b2
Thomas Monfre <tmonfre1@gmail.com>
2020-11-14 17:27:10 -0500

    chore: add link to helper function

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -1,6 +1,7 @@
 import React, { useEffect, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 
+// adopted from: https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
 function ScrollToTop({ history, children }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
