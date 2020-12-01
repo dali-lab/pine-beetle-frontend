b6eea93783be45bf24f8053ce7648f9c2e3d4462
Thomas Monfre <tmonfre1@gmail.com>
2020-11-30 23:39:27 -0600

    fix: clear county and RD when changing state

--- src/state/reducers/selections.js
+++ src/state/reducers/selections.js
@@ -32,7 +32,12 @@ const SelectionsReducer = (state = initialState, action) => {
       };
 
     case ActionTypes.SET_STATE:
-      return { ...state, state: action.payload.state };
+      return {
+        ...state,
+        state: action.payload.state,
+        county: action.payload.state !== state.state ? '' : state.county,
+        rangerDistrict: action.payload.state !== state.state ? '' : state.rangerDistrict,
+      };
 
     case ActionTypes.SET_COUNTY:
       return { ...state, county: action.payload.county };
