5f1c3ddda286bc59bb8dce158aca759ccfe7e98a
Thomas Monfre <tmonfre1@gmail.com>
2020-12-01 15:57:36 -0600

    feat: error/success states for admin file uploading

--- src/screens/admin/components/file-upload/component.js
+++ src/screens/admin/components/file-upload/component.js
@@ -15,51 +15,85 @@ const FileUpload = () => {
 
   const [isUploadingFile, setIsUploadingFile] = useState(false);
   const [uploadingFileError, setUploadingFileError] = useState('');
+  const [successMessage, setSuccessMessage] = useState({});
 
-  const uploadFile = async (uploadFunction, file, clearFile) => {
+  const clearSuccessMessage = () => setSuccessMessage({});
+
+  const clearError = () => {
+    setUploadingFileError('');
+    setCountySpotFile();
+    setRdSpotFile();
+    setUnsummarizedFile();
+    setIsUploadingFile(false);
+    setSuccessMessage({});
+  };
+
+  /**
+   * @description uploads given file
+   * @param {Function} uploadFunction function to upload file
+   * @param {File} file file object
+   * @param {Function} clearFile function to clear the file
+   * @param {String} id file id
+   */
+  const uploadFile = async (uploadFunction, file, clearFile, id) => {
     setIsUploadingFile(true);
 
     try {
       await uploadFunction(file);
       clearFile();
+      setSuccessMessage({ [id]: 'Successfully uploaded file' });
+      setTimeout(clearSuccessMessage, 1000 * 7);
     } catch (err) {
-      setUploadingFileError(err?.response?.data?.error?.message || '');
+      const { data, status } = err?.response || {};
+
+      const strippedError = data?.error.toString().replace('Error: ', '');
+
+      const badRequest = status === 400;
+      const badColumnNames = strippedError.includes('missing fields in csv');
+
+      if (badColumnNames) setUploadingFileError('Incorrect column names. Please upload a different CSV.');
+      else if (badRequest) setUploadingFileError(`Bad request: ${strippedError}`);
+      else setUploadingFileError(strippedError || data?.error.toString() || 'We encountered an error. Please try again.');
     } finally {
       setIsUploadingFile(false);
     }
   };
 
   const componentsToRender = [{
-    name: 'Upload File for County Spot Data',
-    id: 'county-spot',
     file: countySpotFile,
+    id: 'county-spot',
+    name: 'Upload File for County Spot Data',
     selectFile: setCountySpotFile,
-    uploadFile: () => uploadFile(uploadCountySpotCsv, countySpotFile, setCountySpotFile),
+    uploadFile: () => uploadFile(uploadCountySpotCsv, countySpotFile, setCountySpotFile, 'county-spot'),
   }, {
-    name: 'Upload File for Ranger District Spot Data',
-    id: 'rd-spot',
     file: rdSpotFile,
+    id: 'rd-spot',
+    name: 'Upload File for Ranger District Spot Data',
     selectFile: setRdSpotFile,
-    uploadFile: () => uploadFile(uploadRangerDistrictSpotCsv, rdSpotFile, setRdSpotFile),
+    uploadFile: () => uploadFile(uploadRangerDistrictSpotCsv, rdSpotFile, setRdSpotFile, 'rd-spot'),
   }, {
-    name: 'Upload File for Survey123 Unsummarized Data',
-    id: 'unsummarized',
     file: unsummarizedFile,
+    id: 'unsummarized',
+    name: 'Upload File for Survey123 Unsummarized Data',
     selectFile: setUnsummarizedFile,
-    uploadFile: () => uploadFile(uploadSurvey123UnsummarizedCsv, unsummarizedFile, setUnsummarizedFile),
+    uploadFile: () => uploadFile(uploadSurvey123UnsummarizedCsv, unsummarizedFile, setUnsummarizedFile, 'unsummarized'),
   }];
 
   if (isUploadingFile) {
-    return <p>Uploading...</p>;
+    return (
+      <div className="uploading-message-container">
+        <h3>Uploading File...</h3>
+      </div>
+    );
   }
 
   if (uploadingFileError) {
     return (
-      <div id="uploading-error">
-        <p>{uploadingFileError}</p>
+      <div id="uploading-error-container" className="uploading-message-container">
+        <h3>{uploadingFileError}</h3>
         <button
           type="button"
-          onClick={() => setUploadingFileError('')}
+          onClick={clearError}
         >Clear Error
         </button>
       </div>
@@ -71,13 +105,12 @@ const FileUpload = () => {
       {componentsToRender.map(component => (
         <div id={component.id} key={component.id}>
           <p>{component.name}</p>
-          <p
-            id="file-selected"
-          >
+          <p id="file-selected">
             {component.file ? component.file.name : ''}
           </p>
           {component.file ? (
             <button
+              id="upload-button"
               className="custom-file-upload"
               type="button"
               onClick={component.uploadFile}
@@ -85,15 +118,20 @@ const FileUpload = () => {
               Upload File
             </button>
           ) : (
-            <label htmlFor={`file-upload-${component.id}`} className="custom-file-upload">
-              <input
-                id={`file-upload-${component.id}`}
-                type="file"
-                accept=".csv"
-                onChange={e => component.selectFile(e.target.files[0])}
-              />
-              Select File
-            </label>
+            <>
+              {successMessage[component.id] && (
+              <p id="success-message">{successMessage[component.id]}</p>
+              )}
+              <label htmlFor={`file-upload-${component.id}`} className="custom-file-upload">
+                <input
+                  id={`file-upload-${component.id}`}
+                  type="file"
+                  accept=".csv"
+                  onChange={e => component.selectFile(e.target.files[0]) && clearSuccessMessage()}
+                />
+                Select File
+              </label>
+            </>
           )}
         </div>
       ))}
