6886bf5a5aa16b36bb802f6dfe3c52a157ffbc1d
Thomas Monfre <tmonfre1@gmail.com>
2020-12-02 12:40:44 -0600

    feat: add download data to predictions page

--- src/screens/trapping-data/components/index.js
+++ src/screens/trapping-data/components/index.js
@@ -1,13 +1,11 @@
-import DownloadTrapping from './download-trapping';
-import TrappingDataMap from './trapping-data-map';
 import LineChart from './line-chart';
 import OverviewText from './overview-text';
 import SelectionBar from './selection-bar';
+import TrappingDataMap from './trapping-data-map';
 
 export {
-  DownloadTrapping,
-  TrappingDataMap,
   LineChart,
   OverviewText,
   SelectionBar,
+  TrappingDataMap,
 };
