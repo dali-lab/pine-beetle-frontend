e80ed69ba7b5f8a0942398a98b00500cd1a3d601
Thomas Monfre <tmonfre1@gmail.com>
2020-12-03 15:03:27 -0600

    chore: restrict to prod

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ import App from './app';
 import './style.scss';
 
 // google analytics
-ReactGA.initialize('UA-133847731-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
+if (window.location.origin === 'https://pine-beetle-prediction.netlify.app') {
+  ReactGA.initialize('UA-133847731-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 // this creates the store with the reducers, and does some other stuff to initialize devtools
 // boilerplate to copy, don't have to know
