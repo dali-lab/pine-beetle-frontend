7c415e561dd8ddb68962d607dbf5cfd6531dcb79
Jeff Liu <jeffzyliu@gmail.com>
2023-06-06 18:49:01 -0400

    chore: document changes in mbtiles

--- mapbox-tiles/rd-mbtile.sh
+++ mapbox-tiles/rd-mbtile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl https://data.fs.usda.gov/geodata/edw/edw_resources/shp/S_USA.RangerDistrict.zip --output S_USA.RangerDistrict.zip
+# these lines are out of date but may be used in the future
 
-unzip S_USA.RangerDistrict.zip
-
-ogr2ogr -f GeoJSON RD.geojson S_USA.RangerDistrict.shp
+# curl https://data.fs.usda.gov/geodata/edw/edw_resources/shp/S_USA.RangerDistrict.zip --output S_USA.RangerDistrict.zip
+# unzip S_USA.RangerDistrict.zip
+# ogr2ogr -f GeoJSON RD.geojson S_USA.RangerDistrict.shp
 
 # Z3 means minimum zoom-out to level 3, z14 is max zoom-in to level 14. staying consistent with county layer.
 tippecanoe -o RD.mbtiles -Z3 -z14 RD.geojson
