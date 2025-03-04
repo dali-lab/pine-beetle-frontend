4dee054b3d6716f0ed9a5b29a954cb5a2d55f9bd
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2025-03-04 11:55:49 +0100

    feat: delete user functionality enhancements

--- src/screens/admin/components/users/component.js
+++ src/screens/admin/components/users/component.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import DeleteModal from '../delete-modal';
 import trashCan from '../../../../assets/icons/trash-can.png';
-import { deleteAdminUser, getAllAdminUsers } from '../../../../services/admin';
+import { deleteAdminUser } from '../../../../services/admin';
 
 import './style.scss';
 
-const Users = () => {
-  const [users, setUsers] = useState([]);
+const Users = ({ users, setUsers, activeUser }) => {
   const [selectedUser, setSelectedUser] = useState({});
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const allUsers = await getAllAdminUsers();
-
-      setUsers(allUsers.map((user) => ({
-        email: user.email,
-        name: `${user.first_name} ${user.last_name}`,
-        id: user._id,
-      })));
-    })();
-  }, [selectedUser]);
-
   const handleDelete = async (userId) => {
-    await deleteAdminUser(userId);
+    const response = await deleteAdminUser(userId);
+
+    if (response?.status === 200) {
+      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+    }
+
     setSelectedUser({});
   };
 
@@ -36,20 +28,25 @@ const Users = () => {
   return (
     <>
       <p id="users-title">Users</p>
-      {users.map((user) => (
-        <div id="user-info-container" key={user.email}>
-          <div>
-            <p id="user-name">{user.name}</p>
-            <p id="user-email">{user.email}</p>
+      {users.map((user) => {
+        const isActiveUser = activeUser.email === user.email;
+        return (
+          <div id="user-info-container" key={user.email}>
+            <div>
+              <p id="user-name">{user.name}</p>
+              <p id="user-email">{user.email}</p>
+            </div>
+            {!isActiveUser && (
+            <button type="button" className="animated-button delete-user-button" onClick={() => onClickDeleteButton(user)}>
+              <img
+                src={trashCan}
+                alt="trash can icon"
+              />
+            </button>
+            )}
           </div>
-          <button type="button" className="animated-button delete-user-button" onClick={() => onClickDeleteButton(user)}>
-            <img
-              src={trashCan}
-              alt="trash can icon"
-            />
-          </button>
-        </div>
-      ))}
+        );
+      })}
       <DeleteModal
         handleDelete={() => handleDelete(selectedUser.id)}
         isOpen={showDeleteModal}
