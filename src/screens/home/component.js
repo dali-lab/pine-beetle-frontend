8495382c68916e1f8b4aa984c676782f109bff16
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-10-10 14:51:56 +0200

    Make map fullscreen

--- src/screens/home/component.js
+++ src/screens/home/component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FilterBar, Loading } from '../../components';
+import { FilterBar, Footer, Loading } from '../../components';
 import {
   MapWithControls,
 } from './components';
@@ -11,35 +11,18 @@ const Home = (props) => {
   const { isLoading } = props;
 
   return (
-    <>
+    <div className="home-page-wrapper">
       <div className="container">
         <Loading visible={isLoading} />
-      </div>
-      <div className="home-hero-section">
-        <div className="hero-content">
-          <div className="hero-content-inner">
-            <h1 className="hero-title">
-              Southern Pine Beetle
-            </h1>
-
-            <p className="hero-subtitle">
-              Annual Outbreak Predictions and Historical Database (1988–present)
-            </p>
-
-            <p className="hero-description">
-              This website predicts the likelihood of a summer outbreak based on spring trapping data, with the goal of
-              assisting forest managers as they make resource allocation decisions.
-            </p>
-          </div>
-        </div>
       </div>
       <div className="filter-bar-wrapper">
         <FilterBar />
       </div>
-      <div className="full-width-map">
+      <div className="fullscreen-map">
         <MapWithControls />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
