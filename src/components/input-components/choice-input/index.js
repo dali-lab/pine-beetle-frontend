ae5c76a89f476febce0e96be1f49202ef3654516
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-10-30 10:46:26 +0100

    Input style improvements

--- src/components/input-components/choice-input/index.js
+++ src/components/input-components/choice-input/index.js
@@ -68,16 +68,16 @@ const ChoiceInput = (props) => {
           {displayText}
         </span>
         <svg
-          className={`choice-input-arrow ${isOpen ? 'open' : ''}`}
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
+          className="choice-input-arrow"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M5 7.5L10 12.5L15 7.5"
-            stroke="#9ca3af"
+            d="M4 6L8 10L12 6"
+            stroke="#73767e"
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
