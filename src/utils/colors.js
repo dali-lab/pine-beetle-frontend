4d37a6334e792270c362b8172ee7f4fac7dae7a7
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2025-12-18 11:35:45 +0100

    clean colors code

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,20 +1,12 @@
 /* eslint-disable prefer-destructuring */
-// const colors = [
-//   '#86CCFF',
-//   '#FFC148',
-//   '#FFA370',
-//   '#FF525C',
-//   '#CB4767',
-//   '#6B1B38',
-// ];
 
 export const colors = [
-  '#649AD3', // Moderately muted sky blue (#86CCFF)
-  '#E6B951', // Soft golden (#FFC148)
-  '#E89876', // Warm muted orange (#FFA370)
-  '#D95557', // Muted coral red (#FF525C)
-  '#AC5E7D', // Dusty magenta (#CB4767)
-  '#783F58', // Muted deep plum (#6B1B38)
+  '#649AD3',
+  '#E6B951',
+  '#E89876',
+  '#D95557',
+  '#AC5E7D',
+  '#783F58',
 ];
 
 const colorNames = [
