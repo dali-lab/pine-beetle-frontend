e804f81c81ab5a297a13a617ca8cde43a92cb60a
Ewelina Sarad-Cichuta <evelinasarad@gmail.com>
2026-05-27 15:56:13 +0200

    fix(legend): restore missing red color in outbreak probability legend

--- src/screens/home/components/prediction-map/constants.js
+++ src/screens/home/components/prediction-map/constants.js
@@ -1,8 +1,2 @@
 export const thresholds = ['0-2.5%', '2.5-5%', '5-15%', '15-25%', '25-40%', '40-100%'];
-export const colors = [
-  '#649AD3',
-  '#E6B951',
-  '#E89876',
-  '#AC5E7D',
-  '#783F58',
-];
+export { colors } from '../../../../utils/colors';
